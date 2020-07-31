7a4cbd0f50fc169e9751e4a2d88f0b9b9bc0f886
wuliya <wuliya@iplas.com.cn>
2020-07-31 19:36:26 +0800

    jedate组件无法清空的问题修改

--- behaviors/format-behavior.js
+++ behaviors/format-behavior.js
@@ -14,27 +14,54 @@ export const FormatBehavior = {
    */
   formatDate: function (date, fmt, emptyReturn) {
     if (!date && emptyReturn) {
-      return emptyReturn;
+      return emptyReturn
     }
-    date = date || new Date().getTime();
-    fmt = fmt || 'YYYY-MM-DD hh:mm:ss';
-    date = new Date(Number(date));
-    let o = {
-      "M+": date.getMonth() + 1, // 月份   
-      "D+": date.getDate(), // 日  
-      "h+": date.getHours(), // 小时   
-      "m+": date.getMinutes(), // 分   
-      "s+": date.getSeconds(), // 秒    
-      "q+": Math.floor((date.getMonth() + 3) / 3), // 季度   
-      "z": date.getMilliseconds()   // 毫秒   
-    };
+    date = date || new Date().getTime()
+    fmt = fmt || 'YYYY-MM-DD hh:mm:ss'
+    date = new Date(Number(date))
+    const o = {
+      'M+': date.getMonth() + 1, // 月份
+      'D+': date.getDate(), // 日
+      'h+': date.getHours(), // 小时
+      'm+': date.getMinutes(), // 分
+      's+': date.getSeconds(), // 秒
+      'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+      z: date.getMilliseconds() // 毫秒
+    }
     if (/(Y+)/.test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+      fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
     }
-    for (let k  in  o)
-      if (new RegExp("(" + k + ")").test(fmt)) {
-        fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    for (const k in o) {
+      if (new RegExp('(' + k + ')').test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
       }
-    return fmt;
+    }
+    return fmt
+  },
+  _reMatch: function (str) {
+    let smarr = []
+    let maStr = ''
+    const parti = /(^\w{4}|\w{2}\B)/g
+    if (isFinite(str)) {
+      maStr = str.replace(parti, '$1-')
+    } else {
+      maStr = /^[A-Za-z]+$/.test(str) ? str.replace(parti, '$1-') : str
+    }
+    smarr = this._each(maStr)
+    return smarr
+  },
+  _each: function (maStr) {
+    return maStr.match(/\w+|d+/g).map(val => {
+      return isFinite(val) ? parseInt(val) : val
+    })
+  },
+  /**
+   * 将日期转化成时间戳
+   * @param date 日期
+   * @return emptyReturn 默认空值返回数据
+   */
+  dateToTimestamp: function (date) {
+    const arrs = this._reMatch(date)
+    return arrs.length > 0 ? +new Date(arrs[0], arrs[1], arrs[2] || 0, arrs[3] || 0, arrs[4] || 0, arrs[5] || 0) : ''
   }
-};
+}
